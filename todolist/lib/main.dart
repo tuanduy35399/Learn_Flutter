2b135a58ba1a4ff129b3ad1f49618ef289ab066a
Duy <gochoctapcuatuanduy@gmail.com>
2026-06-15 23:15:12 +0700

    learn more about stateless and stateful

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -10,7 +10,7 @@ void main() {
           title: Text("Todolist"),
           leading: Icon(Icons.menu),
         ),
-          body: Center(child: MyApp(true))
+          body: Center(child: MyApp2(false))
       ),
     ),
     debugShowCheckedModeBanner: false,
@@ -26,4 +26,55 @@ class MyApp extends StatelessWidget {
     return loading? CircularProgressIndicator() : const Text("State");
   }
 }
+class MyApp2 extends StatefulWidget {
+  final bool isLoading;
+  const MyApp2(this.isLoading); //khi có truyền tham số
+  //hàm construct bắt buộc thêm required this. neu co truyen dich danh ten cua tham so
+  @override
+  State<MyApp2> createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+
+  late bool _localLoading; //đánh dấu late để tạm thời chưa gán giá trị
+  @override
+  void initState() {
+    // super.initState();
+    _localLoading = widget.isLoading;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return
+      Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if(_localLoading)
+              Padding(
+                padding: EdgeInsets.only(bottom: 16),
+                child:Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.all(Radius.circular(12)),
+                  ),
+                  color: Colors.blueAccent,
+                  child: Padding(padding: EdgeInsetsGeometry.all(16),
+                  child: Text("Loading..."),),
+                )
+              ),
+            FloatingActionButton.extended(onPressed: onClick , label: Text("Push me")),
+          ],
+        ),
+      );
+
+
+  }
+
+  void onClick() {
+    setState(() {
+      _localLoading = !_localLoading;
+    });
+  }
+}
+
 
